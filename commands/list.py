8a1e4aaf2ba5635ca97d84ab5c71ff53d4e1e67d
Georg Donner <georgdonner98@gmail.com>
2018-03-23 15:37:36 +0100

    Remove index info from todo list. Improve filtering.

--- commands/list.py
+++ commands/list.py
@@ -7,21 +7,19 @@ api = TodoistAPI(os.environ['TODOIST_TOKEN'])
 api.sync()
 
 def list_items(args):
-    items = api.state['items'] # all items
+    items = [i for i in api.state['items'] if i['checked'] == 0]
     if not args.all:
         filtered_projects = [proj for proj in api.state['projects'] if proj['name'].lower() == args.project.lower()]
         if len(filtered_projects) > 0:
             project_id = filtered_projects[0]['id']
-            def is_relevant(item, project):
-                return item['project_id'] == project_id and item['checked'] == 0
-            items = [item for item in api.state['items'] if is_relevant(item, project_id)]
+            items = [item for item in items if item['project_id'] == project_id]
         else:
             sys.exit('Project not found')
     print_items(items)
 
 def print_items(items):
-    for index, item in enumerate(items):
-        line = Fore.RESET + '{0} - {1}'.format(hex(index)[2:], item['content'])
+    for item in items:
+        line = Fore.RESET + item['content']
         if item['due_date_utc']:
             due = datetime.strptime(item['due_date_utc'], '%a %d %b %Y %H:%M:%S %z')
             if (due - datetime.now(timezone.utc)) < timedelta(1):
